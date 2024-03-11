569aa12287063c97a8a19f7e4a989454266b8f48
Lev Aleshkin <lev.aleshkin01@yandex.ru>
2024-03-11 17:48:52 +0300

    Commit_11.03.2024.17:48

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,9 +1,21 @@
 ﻿namespace MyConsoleApp;
+
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.ReadLine();
+        //Console.WriteLine("Hello!");
+        //Console.ReadLine();
+        List<int> exList = new List<int>();
+        var exList_1 = new List<int>{1,2,3};
+
+        for(int i = 0; i <= 10; i++) //1000000
+        {
+            exList.Add(i);
+            i++;
+            //Console.WriteLine(exList);
+            PrintList(exList);
+        }
     }
 }
