83105a297c58c1a790ff5b98291945953619b970
Lex <lev.aleshkin01@yandex.ru>
2024-03-17 18:32:13 +0300

    Commit_17.03.2024_18:31

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,82 +1,83 @@
 ﻿using System.Collections;
 using System.Diagnostics;
-
 namespace MyConsoleApp;
-
-
-
-
 
 class Program
 {
     static void Main(string[] args)
     {
-        List<int>       exList = new List<int>();
-        ArrayList       myAL = new ArrayList();
-        LinkedList<int> myLL = new LinkedList<int>();
-        Stopwatch       stopwatch = new Stopwatch(),
-                        stopwatchElementList = new Stopwatch(),
-                        stopwatchElementArrayList = new Stopwatch(),
-                        stopwatchElementLinkedList = new Stopwatch();
-        int             elementList, 
-                        searchElement = 496753;
-
+        List<int>       exList          = new List<int>();
+        ArrayList       myAL            = new ArrayList();
+        LinkedList<int> myLL            = new LinkedList<int>();
+        Stopwatch       stopwatch       = new Stopwatch();
+        int             elementList,
+                        size            = 1000000,
+                        searchElement   = 496753,
+                        remainderNumber = 777;
+        
+        //"List:"
         Console.WriteLine("List:");
         stopwatch.Start();
-        for(int i = 0; i <= 1000000; i++) //1000000
-        {           
-            exList.Add(i);           
-            //Console.WriteLine(exList[i]);          
-        }  
+        for(int i = 0; i <= size; i++)         
+            exList.Add(i);                          
+
         stopwatch.Stop();      
         Console.WriteLine("Прошедшее время заполнения в миллисекундах: {0}", stopwatch.ElapsedMilliseconds); 
         
-        //stopwatchElementList.Start();
-        stopwatchElementList.Start();
+        stopwatch.Restart();
         elementList = exList[searchElement];
-        stopwatchElementList.Stop();
-        //stopwatchElementList.Stop();
-        Console.WriteLine("Прошедшее время поиска элемента в миллисекундах: {0}", stopwatchElementList.ElapsedMilliseconds);
+        stopwatch.Stop();
+        Console.WriteLine("Прошедшее время поиска элемента {0} в миллисекундах: {1}", searchElement, stopwatch.ElapsedMilliseconds);
 
-        Console.WriteLine("ArrayList:");
-        stopwatch.Start();
-        for(int i = 0; i <= 1000000; i++) //1000000
-        {            
-            myAL.Add(i);                        
-            //Console.WriteLine(myAL[i]);
+        stopwatch.Restart();
+        foreach (int item in exList)
+        {
+            if(item % remainderNumber == 0)
+                Console.WriteLine(item);          
         }
         stopwatch.Stop();
-        Console.WriteLine("Прошедшее время заполнения в миллисекундах: {0}", stopwatch.ElapsedMilliseconds);    
+        Console.WriteLine("Прошедшее время заполнения foreach в миллисекундах: {0}", stopwatch.ElapsedMilliseconds);
 
-        //stopwatchElementArrayList.Start();
-        stopwatchElementList.Start();
+        //"ArrayList:"
+        Console.WriteLine("ArrayList:");
+        stopwatch.Restart();
+        for(int i = 0; i <= size; i++)          
+            myAL.Add(i);                        
+        stopwatch.Stop();
+        Console.WriteLine("Прошедшее время заполнения в миллисекундах: {0}", stopwatch.ElapsedMilliseconds);   
+
+        stopwatch.Restart();
         elementList = (int)myAL[searchElement];
-        //stopwatchElementArrayList.Stop();
-        stopwatchElementList.Stop();
-        Console.WriteLine("Прошедшее время поиска элемента в миллисекундах: {0}", stopwatchElementList.ElapsedMilliseconds);
+        stopwatch.Stop();
+        Console.WriteLine("Прошедшее время поиска элемента {0} в миллисекундах: {1}", searchElement, stopwatch.ElapsedMilliseconds);
 
-        Console.WriteLine("LinkedList:");
-        //LinkedListNode<int> currentNode = myLL.First;   
-        stopwatch.Start();     
-        for(int i = 0; i <= 1000000; i++) //1000000
-        {            
+        stopwatch.Restart();
+        foreach (int item in myAL)
+        {
+            if(item % remainderNumber == 0)
+                Console.WriteLine(item);        
+        }
+        stopwatch.Stop();
+        Console.WriteLine("Прошедшее время заполнения foreach в миллисекундах: {0}", stopwatch.ElapsedMilliseconds);
+
+        //"LinkedList:"
+        Console.WriteLine("LinkedList:");   
+        stopwatch.Restart();     
+        for(int i = 0; i <= size; i++)           
             myLL.AddLast(i);             
-            //Console.WriteLine(currentNode.Next);
+
+        foreach (int item in myLL)
+        {
+            elementList = item;
+            if(elementList % remainderNumber == 0)   
+                Console.WriteLine(item);    
         }
         stopwatch.Stop();
         Console.WriteLine("Прошедшее время заполнения в миллисекундах: {0}", stopwatch.ElapsedMilliseconds); 
 
-        //stopwatchElementLinkedList.Start();
-        stopwatchElementList.Start();
-        //elementList = myLL.Select(searchElement);
+        stopwatch.Restart();
         elementList = myLL.ElementAt(searchElement);
-        //stopwatchElementLinkedList.Stop();
-        stopwatchElementList.Stop();
-        Console.WriteLine("Прошедшее время поиска элемента в миллисекундах: {0}", stopwatchElementList.ElapsedMilliseconds);
-
-        // foreach (int item in myLL)
-        // {
-        //     Console.WriteLine(item);
-        // }
+        stopwatch.Stop();
+        Console.WriteLine("Прошедшее время поиска элемента {0} в миллисекундах: {1}", searchElement, stopwatch.ElapsedMilliseconds);
     }
 }
